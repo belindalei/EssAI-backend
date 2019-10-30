64622dd6bba6b43348ac258757cc3636d45fb3e1
Belinda Lei <belinda.lei95@gmail.com>
2019-10-30 15:05:02 -0400

    added auth to backend

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,6 +4,11 @@ class Api::V1::UsersController < ApplicationController
     @users = User.all 
     render json: @users
   end
+
+  def show
+      user = User.find(params[:id])
+      render json: {user: UserSerializer.new(user)}
+  end
   
   def create
     user = User.create(user_params)
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   
-namespace :api do 
-  namespace :v1 do 
-    resources :responses
-    resources :essays
-    resources :users
-    get '/autologin', to: 'auth#create'
-    post 'login', to: 'auth#login'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  end 
+  namespace :api do 
+    namespace :v1 do 
+      resources :responses
+      resources :essays
+      resources :users
+      get '/autologin', to: 'auth#create'
+      post 'login', to: 'auth#login'
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    end 
+  end
+  
 end
